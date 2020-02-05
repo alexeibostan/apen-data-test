0cfd1ca225da287dea1ca6bf5e795c442bffe4fe
Alexei Bostan <alexei-bostan@dedagroup.eu>
2020-02-05 17:03:44 +0100

    Updates

---
+++ precache-manifest.eeecd57ed58ddd101ab2bd69d0da6139.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e17e3867648080845c8833c25fdb458",
+    "url": "/apen-data-test/index.html"
+  },
+  {
+    "revision": "3f6451ee1d23a96cbf41",
+    "url": "/apen-data-test/static/css/main.4eeaaaae.chunk.css"
+  },
+  {
+    "revision": "816eef87d6a0d1cef1b9",
+    "url": "/apen-data-test/static/js/2.05a8e298.chunk.js"
+  },
+  {
+    "revision": "f7471f80dde77df5a626a4225cf9d77a",
+    "url": "/apen-data-test/static/js/2.05a8e298.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3f6451ee1d23a96cbf41",
+    "url": "/apen-data-test/static/js/main.487f79c8.chunk.js"
+  },
+  {
+    "revision": "da6a9d3c306f670c6f7e",
+    "url": "/apen-data-test/static/js/runtime-main.1acba30c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/apen-data-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
